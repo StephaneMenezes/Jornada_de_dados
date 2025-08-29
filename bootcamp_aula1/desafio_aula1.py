8e1ac6a8a63911886368e786394bb78525b4b9c6
Stephane <stephanemenezes@fisica.ufc.br>
2025-08-28 21:10:54 -0300

    Adicionando bootcamp_aula1 como pasta normal

---
+++ bootcamp_aula1/desafio_aula1.py
@@ -0,0 +1,20 @@
+# Solicite ao usuário que digite seu nome:
+
+nome_user = input("Digite seu nome: ")
+
+# Solicite ao usuário que digite o valor do seu salário: 
+
+salario_user = float(input("Digite o valor do seu salário: R$ "))
+
+# Solicite ao usuário que digite o valor do bônus recebido
+
+bonus_user = float(input("Digite o valor do bônus recebido: R$ "))
+
+valor_total = salario_user + bonus_user
+
+# Imprima o cálculo do KPI: 
+print( f"O cálculo do KPI é calculado como: Salário Mensal R$ {salario_user} + Bônus recebido R$ {bonus_user} = R$ {valor_total}")
+
+print(f"Olá {nome_user}, o seu recebimento total será de R$ {valor_total:.2f} reais. Resumo: Salário Mensal R$ {salario_user} + Bônus recebido R$ {bonus_user}")
+
+#Desafio_Aula 1
---
+++ bootcamp_aula1/exemplo_00.py
@@ -0,0 +1,15 @@
+print("Hello World" + " " + "Stephane" + "!")
+
+
+
+#Crie um programa que pergunta ao usuário o seu nome e ele retorna quantos caracteres o nome tem: 
+
+nome = input("Qual o seu nome?")
+print(f"Olá {nome}, o seu nome tem: {len(nome)} caracteres")
+
+# Crie um programa que pergunta ao usuário dois valores e mostre a soma deles:
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um outro número: "))
+
+print(f"A soma entre o {num1} e {num2} é igual a : {num1 + num2}")
